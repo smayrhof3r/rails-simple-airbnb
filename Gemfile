bb25fe654562d5d5903f426b19b58dd64d741d1f
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-07-21 19:59:04 +0100

    index page complete

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+gem 'simple_form', github: 'heartcombo/simple_form'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+
+100.times do
+  Flat.create!({
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: (50..200).to_a.sample,
+    number_of_guests: (2..10).to_a.sample
+  })
+  end
